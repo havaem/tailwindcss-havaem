b926e7867d93c35ce017d107e617160dc66d5ac0
havaem <vhnvohoainam@gmail.com>
2023-09-05 14:27:06 +0700

    feat: 🎸 update src

---
+++ dist/index.js
@@ -0,0 +1,43 @@
+import plugin from 'tailwindcss/plugin';
+const havaem = plugin(function ({ addUtilities }) {
+    addUtilities({
+        '.flex-center': {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+        '.flex-column': {
+            display: 'flex',
+            flexDirection: 'column',
+        },
+        '.flex-row': {
+            display: 'flex',
+            flexDirection: 'row',
+        },
+        '.flex-center-x': {
+            display: 'flex',
+            justifyContent: 'center',
+        },
+        '.flex-center-y': {
+            display: 'flex',
+            alignItems: 'center',
+        },
+        '.absolute-center': {
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+        },
+        '.absolute-center-x': {
+            position: 'absolute',
+            left: '50%',
+            transform: 'translate(-50%, 0)',
+        },
+        '.absolute-center-y': {
+            position: 'absolute',
+            top: '50%',
+            transform: 'translate(0, -50%)',
+        },
+    });
+}, {});
+module.exports = havaem;
